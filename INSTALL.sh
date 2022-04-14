896cb705c23a9645245bac59e59266b2a825e632
Nick Engmann <cyengmann@gmail.com>
2022-04-14 15:34:08 -0400

    fix install and add camera app

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# install rclone
+# Install Rclone
+echo "[DEBUG]:Install Rclone:"
 curl https://rclone.org/install.sh | sudo bash
+mkdir -p /home/pi/drive/Garage_Videos
+
+# [TODO!]still need to configure rclone (which could be done via the web-interface?)
+# rclone config
 
 # Fix the udev rules
+echo "[DEBUG]:Fix the udev rules:"
 sudo cp 99-usb-autorun.rules /etc/udev/rules.d/
 sudo install -m 644 *.service /etc/systemd/system/ 
 install -m 777 *.sh /usr/local/bin
@@ -11,11 +17,14 @@ sudo udevadm control --reload && udevadm trigger
 sudo systemctl start rclone-automount
 sudo systemctl enable rclone-automount
 
-# install raspberry wifi config
+#  Install Raspberry-Wifi-Config
+echo "[DEBUG]:Install Raspberry-Wifi-Config:"
 cd /home/pi
-git clone https://github.com/sabhiram/raspberry-wifi-conf.git
+git clone https://github.com/NickEngmann/raspberry-wifi-conf.git
 cd raspberry-wifi-conf
-# install npm and node
+
+# Install npm and node as well as Raspberry-Wifi-Config dependencies
+echo "[DEBUG]:Install npm and node as well as Raspberry-Wifi-Config dependencies:"
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt-get install -y nodejs
 sudo npm install bower -g
@@ -23,10 +32,9 @@ npm update
 bower install
 sudo npm run-script provision
 sudo npm start
-mkdir /home/pi/drive/
-mkdir /home/pi/drive/Garage_Videos
 
-# set up raspberry wifi config app as a service
+# Set up Raspberry-Wifi-Config app as a service
+echo "[DEBUG]:Set up Raspberry-Wifi-Config app as a service:"
 sudo cp assets/init.d/raspberry-wifi-conf /etc/init.d/raspberry-wifi-conf 
 sudo chmod +x /etc/init.d/raspberry-wifi-conf  
 sudo update-rc.d raspberry-wifi-conf defaults
@@ -34,11 +42,13 @@ sudo systemctl unmask hostapd # by default the hostapd is masked so we need to u
 sudo systemctl enable hostapd
 sudo systemctl start hostapd
 
-# edit rc.local to add the following lines before the exit 0
+# [TODO!] Edit rc.local to add the following lines before the exit 0
 # cd /home/pi/raspberry-wifi-conf
 # sudo /usr/bin/node server.js < /dev/null &
 
-# add libcamera and Arducam to the system
+# Adding libcamera and Arducam to the system
+echo "Adding `libcamera` and Arducam to the system:"
+cd ~
 wget -O install_pivariety_pkgs.sh https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/install_script/install_pivariety_pkgs.sh
 chmod +x install_pivariety_pkgs.sh
 ./install_pivariety_pkgs.sh -p libcamera_dev
@@ -47,10 +57,32 @@ chmod +x install_pivariety_pkgs.sh
 git clone https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver.git
 
 # Open /boot/config.txt and add "dtoverlay=vc4-fkms-v3d" under [all] and then save and reboot
+sudo sed -i -e '$i \dtoverlay=vc4-fkms-v3d\n' /boot/config.txt
 
-# Install miniTFT display
-https://learn.adafruit.com/adafruit-mini-pitft-135x240-color-tft-add-on-for-raspberry-pi/1-3-240x240-kernel-module-install
-# ^remember to change a line in the libts installation to include installing libts-dev, i.e.: sudo apt install libts-dev
-# this means I am going to have to fork the repo and make the change inline and add all the instructions
+# Installing miniTFT display
+echo "[DEBUG]:Install miniTFT display:"
+cd ~
+sudo pip3 install --upgrade adafruit-python-shell click
+sudo apt-get install -y git
+git clone https://github.com/NickEngmann/Raspberry-Pi-Installer-Scripts.git
+cd Raspberry-Pi-Installer-Scripts
+sudo python3 adafruit-pitft.py --display=st7789_240x240 --rotation=1 --install-type=fbcp
 
+# Install packages related to the Moment python executable
+echo "[DEBUG]:Install packages related to the Moment python executable:"
+mkdir -p /home/pi/moment/icon
+cp -rf icon /home/pi/moment
+echo  "[DEBUG]:Installing Pip Requirements"
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+python3 -m pip install --upgrade Pillow
+echo  "[DEBUG]:Installing MomentCamera AutoStart service"
+sudo install Moment.py /usr/bin/
+sudo mkdir -p /home/pi/.config/autostart
+sudo install -m 644 *.desktop /home/pi/.config/autostart/
+sudo sed -i -e '$i \start_x=1\ngpu_mem=128\n' /boot/config.txt
+
+# Reboot
+echo "[SUCCESS]: Installation Finished, Rebooting Now..."
+sleep 5
 sudo reboot now
---
+++ Moment.py
@@ -0,0 +1,168 @@
+#!/usr/bin/python3
+
+from guizero import App, PushButton, Text, Picture, Window
+from time import sleep
+import time
+import glob
+import datetime
+import sys, os
+import subprocess
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+
+
+class Moment:
+    def __init__(self):
+        self.capture_number = self.timestamp()
+        self.video_capture_number = self.timestamp()
+        self.picture_index = 0
+        self.saved_pictures = [] 
+        self.shown_picture = "" 
+      
+        GPIO.setwarnings(False) # Ignore warning for now
+        GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering
+        GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(23, GPIO.FALLING, callback=self.takePicture, bouncetime=2500)
+            
+        self.app = App(layout="grid", title="Camera Controls", bg="black", width=480, height=320)
+
+        text0 = Text(self.app,color="white", grid=[1,0], text="- Moment -")
+
+        button1 = PushButton(self.app, grid=[1,1], width=110, height=110, pady=35, padx=10, image="/home/pi/Moment/icon/prev.png", command=self.long_preview)
+        text1 = Text(self.app, color="white", grid=[1,2],text="Focus")
+
+        button2 = PushButton(self.app, grid=[3,1], width=110, height=110, pady=35, padx=10, image="/home/pi/Moment/icon/gallery.png", command=self.show_gallery)
+        text2 = Text(self.app, color="white", grid=[3,2],text="Gallery")
+
+        button3 = PushButton(self.app, grid=[5,1], width=110, height=110,  pady=35, padx=10, image="/home/pi/Moment/icon/vid.png", command=self.video_capture)
+        text2 = Text(self.app, color="white", grid=[5,2],text="HD 30s")
+
+        button4 = PushButton(self.app, grid=[7,1], width=110, height=110, pady=35, padx=10, image="/home/pi/Moment/icon/lapse.png", command=self.burst)
+        text3 = Text(self.app, color="white", grid=[7,2],text="Burst")
+
+        button5 = PushButton(self.app, grid=[1,3], width=110, height=110, image="/home/pi/Moment/icon/self.png", command=self.lapse)
+        text4 = Text(self.app, color="white", grid=[1,4],text="1h 60pix")
+
+        button6 = PushButton(self.app, grid=[3,3], width=110, height=110, image="/home/pi/Moment/icon/long.png", command=self.split_hd_30m)
+        text2 = Text(self.app, color="white", grid=[3,4],text="HD 30m in 5s")
+
+        button7 = PushButton(self.app, grid=[5,3], width=110, height=110, image="/home/pi/Moment/icon/drop.png", command=self.upload)
+        text3 = Text(self.app, color="white", grid=[5,4],text="Upload")
+
+        button8 = PushButton(self.app, grid=[7,3], width=110, height=110, image="/home/pi/Moment/icon/del.png", command=self.clear)
+        text4 = Text(self.app, color="white", grid=[7,4],text="Clear Folder")
+
+        self.busy = Window(self.app, bg="red",  height=175, width=480, title="busy")
+
+        self.app.tk.attributes("-fullscreen", True)
+        self.busy.hide()
+        self.app.display()
+
+    def clear(self):
+        self.show_busy()
+        os.system("rm -v /home/pi/Downloads/*")
+        self.hide_busy()
+    
+    def show_busy(self):
+        self.busy.show()
+        print("busy now")
+        
+    def hide_busy(self):
+        self.busy.hide()
+        print("no longer busy")
+        
+    def fullscreen(self):
+        self.app.tk.attributes("-fullscreen", True)
+
+    def notfullscreen(self):
+        self.app.tk.attributes("-fullscreen", False)
+
+    # Generate timestamp string generating name for photos
+    def timestamp(self):
+        tstring = datetime.datetime.now()
+        #print("Filename generated ...")
+        return tstring.strftime("%Y%m%d_%H%M%S")
+  
+    def burst(self):
+        self.show_busy()
+        capture_number = self.timestamp()
+        print("Raspistill starts")
+        os.system("raspistill -t 10000 -tl 0 --thumb none -n -bm -o /home/pi/Downloads/BR" +str(capture_number) + "%04d.jpg")
+        print("Raspistill done")
+        self.hide_busy()
+        
+    def split_hd_30m(self):   
+        self.show_busy()
+        capture_number = self.timestamp()
+        print("Raspivid starts")
+        os.system("raspivid -f -t 1800000 -sg 300000  -o /home/pi/Downloads/" +str(capture_number) + "vid%04d.h264")
+        print("done")
+        self.hide_busy()
+    
+    def lapse(self):
+        self.show_busy()
+        capture_number = self.timestamp()
+        print("Raspistill timelapse starts")
+        os.system("raspistill -t 3600000 -tl 60000 --thumb none -n -bm -o /home/pi/Downloads/TL" +str(capture_number) + "%04d.jpg")
+        print("Raspistill timelapse done")
+        self.hide_busy()
+
+    def long_preview(self):
+        self.show_busy()
+        print("15 second preview")
+        os.system("raspistill -f -t 15000")
+        self.hide_busy()
+
+    def capture_image(self):
+        self.show_busy()
+        capture_number = self.timestamp()
+        print("Raspistill starts")
+        os.system("raspistill -f -o /home/pi/Downloads/" +str(capture_number) + "cam.jpg")
+        print("Raspistill done")
+        self.hide_busy()
+
+    def takePicture(self, channel):
+        print ("Button event callback")
+        capture_number = self.timestamp()
+        print("Raspistill starts")
+        os.system("raspistill -f -t 3500 -o /home/pi/Downloads/" +str(capture_number) + "cam.jpg")
+        print("Raspistill done")
+
+    def picture_left(self):
+        if (self.picture_index == 0):
+            self.pictures = (len(self.saved_pictures) - 1)    
+        self.picture_index -= 1
+        self.shown_picture = self.saved_pictures[self.picture_index]
+        self.picture_gallery = Picture(self.gallery, width=360, height=270, image=self.shown_picture, grid=[1,0])
+
+    def picture_right(self):
+        if (self.picture_index == (len(self.saved_pictures) - 1)): 
+            self.picture_index = 0 
+        self.picture_index += 1
+        self.shown_picture = self.saved_pictures[self.picture_index]
+        self.picture_gallery = Picture(self.gallery, width=360, height=270, image=self.shown_picture, grid=[1,0])
+
+    def show_gallery(self):
+        self.gallery = Window(self.app, bg="white", height=300, width=460, layout="grid",title="Gallery")
+        self.saved_pictures = glob.glob('/home/pi/Downloads/*.jpg')
+        self.shown_picture = self.saved_pictures[self.picture_index] 
+        button_left = PushButton(self.gallery, grid=[0,0], width=40, height=50, pady=50, padx=10, image="/home/pi/Moment/icon/left.png", command=self.picture_left)    
+        self.picture_gallery = Picture(self.gallery, width=360, height=270, image=self.shown_picture, grid=[1,0]) 
+        button_right = PushButton(self.gallery, grid=[2,0], width=40, height=50, pady=50, padx=10, image="/home/pi/Moment/icon/right.png", command=self.picture_right) 
+        self.gallery.show()
+
+    def video_capture(self):
+        self.show_busy()
+        capture_number = self.timestamp()
+        print("Raspivid starts")
+        os.system("raspivid -f -t 30000 -o /home/pi/Downloads/" +str(capture_number) + "vid.h264")
+        print("done")
+        self.hide_busy()
+
+    def upload(self):
+        self.show_busy()
+        subprocess.Popen(["bash", "/home/pi/Moment/usb_autorun.sh", "--yes"])
+        self.hide_busy()
+
+if __name__ == '__main__':
+    standalone_app = Moment()
+    standalone_app.run()
